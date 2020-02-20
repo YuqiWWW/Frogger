d3a8488649b336da8fddf247dd7adf9d28829544
Yuqi <wei56@wisc.edu>
2020-02-20 15:47:41 +0800

    Moving the avatar

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
+    this.y = [68, 151, 234][Math.floor(Math.random()*3)];
+    this.speed = Math.random() * 100;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +17,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + dt * this.speed;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +28,53 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
+    constructor() {
+        this.sprite = 'images/char-boy.png';
+        this.x = 200;
+        this.y = 400;
+    }
+    update (){
+
+    }
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+    handleInput(key) {
+        if (key === 'left' && this.x >= 100) {
+            this.x -= 100;
+        }
+        if (key === 'right' && this.x <= 300) {
+            this.x += 100;
+        }
+        if (key === 'up'  && this.y >= 68) {
+            this.y -= 83;
+        }
+        if (key === 'down' && this.y <= 317) {
+            this.y += 83;
+        }
+        // console.log(this.x, this.y);
+
+        // reset
+        if (this.y <= 0) {
+            this.reset();
+        }
+    }
+    
+    reset() {
+        this.x = 200;
+        this.y = 400;
+    }
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+let allEnemies = [];
+let enemy1 = new Enemy();
+allEnemies.push(enemy1);
+let player = new Player();
 
 
 // This listens for key presses and sends the keys to your
